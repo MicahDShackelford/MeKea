9890aad3e5625314f57ca850d94fe725fcd94d5b
Micah Shackelford <micahdshackelford@gmail.com>
2019-11-29 22:11:43 -0500

    Complete basic Express server (Currently only serving static index.html from client)

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = 3001;
+const PUBLIC_DIR = './client/public/';
+
+app.use('/', express.static(PUBLIC_DIR));
+
+app.listen(port, () => {
+  console.log(`[Server] : Listening on port ${port}`);
+});
