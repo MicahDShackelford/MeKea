ac5063483f5bf5e85cc351619cb96abcc9e15134
Micah Shackelford <micahdshackelford@gmail.com>
2019-12-10 17:40:58 -0500

    Added access origin for proxy

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const PUBLIC_DIR = path.join(__dirname + '/../client/public');
 app.use('/', express.static(PUBLIC_DIR));
 
 app.get('/product/:pid', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   let pid = parseInt(req.params.pid);
   Product.collection.findOne({id: pid}, (err, doc) => {
     if(err) {
